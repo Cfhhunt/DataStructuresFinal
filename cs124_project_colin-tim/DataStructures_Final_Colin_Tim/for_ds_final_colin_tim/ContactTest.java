851a3341237c66bd1f1f3e20cd74f456076d9459
Colin Hunt <cfhhunt@gmail.com>
2018-11-29 14:43:48 -0800

    First commit, adding the locally created java directory

---
+++ cs124_project_colin-tim/DataStructures_Final_Colin_Tim/for_ds_final_colin_tim/ContactTest.java
@@ -0,0 +1,11 @@
+package for_ds_final_colin_tim;
+
+//Tests to check that the other 2 classes work properly
+public class ContactTest {
+
+	public static void main(String[] args) {
+		//Susan provided some of the code on the assignment sheet, import later
+
+	}
+
+}
